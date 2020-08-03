bb4086e77f4f9729ef91e523b331c8b0a5994c81
nyx <786465269@qq.com>
2020-08-03 20:42:02 +0800

    update 200803

--- python/thread and process/process.py
+++ python/thread and process/process.py
@@ -1,6 +1,8 @@
 import os
-from multiprocessing import Pool, Process
+from multiprocessing import Pool, Process, Queue
 import time
+import subprocess
+import random
 
 """linux或unix系统中可以使用fork新起一个子进程"""
 # p = os.fork()
@@ -22,19 +24,46 @@ import time
 #     print('Child process end.')
 
 """进程池"""
-def long_time_task(name):
-    print(f'run task {name} {os.getpid()}')
-    start = time.time()
-    time.sleep(3)
-    end = time.time()
-    print(f'task {name} runs {end - start} seconds')
+# def long_time_task(name):
+#     print(f'run task {name} {os.getpid()}')
+#     start = time.time()
+#     time.sleep(3)
+#     end = time.time()
+#     print(f'task {name} runs {end - start} seconds')
+
+# if __name__ == '__main__':
+#     print(f'Parent process {os.getpid()}.')
+#     process_pool = Pool(5) #Pool默认大小是cpu核数
+#     for i in range(5):
+#         process_pool.apply_async(long_time_task, args=(i,))
+#     print('waiting for all processes end')
+#     process_pool.close() # 确保不会再有新的子进程加入
+#     process_pool.join()
+#     print('all processes done')
+
+"""subprocess启动子进程"""
+# subprocess.call(['df', '-h'])
+
+"""进程间通信，一般使用queue或者pipe进行数据交换"""
+def write(q: Queue):
+    print(f'start write process {os.getpid()}')
+    for n in ['a', 'b', 'c']:
+        print(f'put {n} to queue')
+        q.put(n)
+        time.sleep(random.random())
+
+def read(q: Queue):
+    print(f'start read process {os.getpid()}')
+    while True:
+        value = q.get(True)
+        print(f'get {value} from queue')
+
 
 if __name__ == '__main__':
-    print(f'Parent process {os.getpid()}.')
-    process_pool = Pool(5) #Pool默认大小是cpu核数
-    for i in range(5):
-        process_pool.apply_async(long_time_task, args=(i,))
-    print('waiting for all processes end')
-    process_pool.close() # 确保不会再有新的子进程加入
-    process_pool.join()
-    print('all processes done')
+    q = Queue()
+    pw = Process(target=write, args=(q,))
+    pr = Process(target=read, args=(q,))
+    pw.start()
+    pr.start()
+    pw.join()
+    pr.terminate()
